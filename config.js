caa7391f40f6c7ef0eb04fc5cea98b1bfa5bcc0b
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-03 17:11:33 +0200

    removed wrapper

--- config.js
+++ config.js
@@ -1,25 +1,2 @@
-(function () {
-
-    try {
-        window.TMSHelper.console("[pageview-virtual] start");
-        var secondsToWait = 15;
-        var intervalCounter = 1;
-        intervalLoop = setInterval(function() {
-            if (typeof window.TMSConfig === "Object" || Object.keys(window.TMSConfig).length > 0) {
-                clearInterval(intervalLoop);
-                // config starts here
-                window.TMSConfig['tmsConfig_event_trackDefault'] = true;
-                window.TMSConfig['tmsConfig_cookieDomain'] = 'projuventute.ch';
-                // end: config
-            } else if (intervalCounter >= secondsToWait * 2) { // after X * 2 tries = X seconds, stop the loop
-                clearInterval(intervalLoop);
-            } else {
-                intervalCounter++;
-            }
-        }, 500);
-    } catch (err) {
-        window.TMSHelper.console("[pageview-virtual] error:");
-        window.TMSHelper.errorHandler(err);
-    }
-
-})();
+window.TMSConfig['tmsConfig_event_trackDefault'] = true;
+window.TMSConfig['tmsConfig_cookieDomain'] = 'projuventute.ch';
