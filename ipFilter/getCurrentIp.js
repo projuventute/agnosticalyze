15e60964e47c57404ddbfef7f7df6b7876b4031c
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-04 18:46:42 +0200

    debug

--- ipFilter/getCurrentIp.js
+++ ipFilter/getCurrentIp.js
@@ -1,4 +1,4 @@
-function window.TMSProcessing.getCurrentIp() {
+window.TMSProcessing.getCurrentIp = function () {
     try {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() { 
