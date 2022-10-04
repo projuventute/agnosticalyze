b115dbdd127368c9d3f634b3f91f41b0a09f1f2b
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-04 18:43:57 +0200

    functionized

--- ipFilter/getCurrentIp.js
+++ ipFilter/getCurrentIp.js
@@ -1,13 +1,15 @@
-try {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            var currentIp = JSON.parse(xmlHttp.responseText).ip;
-            // console.log("current IP: " + currentIp);
-            window.userIp = currentIp;
-    }
-    xmlHttp.open( "GET", "https://api.ipify.org?format=json", true ); // false for synchronous request
-    xmlHttp.send( null );        
-} catch (err) {
-    console.error(err);
-};
+function window.TMSProcessing.getCurrentIp() {
+    try {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                var currentIp = JSON.parse(xmlHttp.responseText).ip;
+                // console.log("current IP: " + currentIp);
+                window.userIp = currentIp;
+        }
+        xmlHttp.open( "GET", "https://api.ipify.org?format=json", true ); // false for synchronous request
+        xmlHttp.send( null );        
+    } catch (err) {
+        console.error(err);
+    };
+}
