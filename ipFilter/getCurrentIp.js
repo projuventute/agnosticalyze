cc02014b6068685f2ea89e33768ab6bddbcdc6fd
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-05 08:34:42 +0200

    getIp from local

--- ipFilter/getCurrentIp.js
+++ ipFilter/getCurrentIp.js
@@ -1,15 +1,21 @@
-window.TMSProcessing.getCurrentIp = function () {
+window.TMSHelper.getCurrentIp = function () {
     try {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() { 
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                var currentIp = JSON.parse(xmlHttp.responseText).ip;
-                // console.log("current IP: " + currentIp);
-                window.userIp = currentIp;
+        var userIpFromLocalStorage = window.TMSHelper.readValue('userIp', false);
+        if (typeof userIpFromLocalStorage === 'string' && userIpFromLocalStorage !== '') {
+
+        } else {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() { 
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    var currentIp = JSON.parse(xmlHttp.responseText).ip;
+                    // console.log("current IP: " + currentIp);
+                    // window.userIp = currentIp;
+                    window.TMSHelper.storeValue('userIp', currentIp, 1);
+            }
+            xmlHttp.open( "GET", "https://api.ipify.org?format=json", true ); // false for synchronous request
+            xmlHttp.send( null );
         }
-        xmlHttp.open( "GET", "https://api.ipify.org?format=json", true ); // false for synchronous request
-        xmlHttp.send( null );        
     } catch (err) {
-        console.error(err);
+        window.TMSHelper.console(err);
     };
 }
