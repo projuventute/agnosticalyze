644f749209cba447b0cb0fa3071a8d2232ec5c03
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-05 09:59:40 +0200

    debug

--- ipFilter/getCurrentIp.js
+++ ipFilter/getCurrentIp.js
@@ -4,11 +4,12 @@ window.TMSHelper.getCurrentIp = function () {
         if (typeof userIpFromLocalStorage !== 'string' || userIpFromLocalStorage !== '') {
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange = function() { 
-                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
                     var currentIp = JSON.parse(xmlHttp.responseText).ip;
                     // console.log("current IP: " + currentIp);
                     // window.userIp = currentIp;
                     window.TMSHelper.storeValue('userIp', currentIp, 1);
+
                     // run TMSProcessing.TMSEventOrchestrator after IP is stored
                     // trigger GTM Event
                     try {
@@ -26,7 +27,7 @@ window.TMSHelper.getCurrentIp = function () {
                     }
             }
             xmlHttp.open( "GET", "https://api.ipify.org?format=json", true ); // false for synchronous request
-            xmlHttp.send( null );
+            xmlHttp.send();
         }
     } catch (err) {
         window.TMSHelper.console(err);
