68cb7745b8f1a0c1c45373d1cf71b51f7b0def6b
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2022-10-05 08:48:14 +0200

    pseudoConsent as function

--- ipFilter/getCurrentIp.js
+++ ipFilter/getCurrentIp.js
@@ -1,9 +1,7 @@
 window.TMSHelper.getCurrentIp = function () {
     try {
         var userIpFromLocalStorage = window.TMSHelper.readValue('userIp', false);
-        if (typeof userIpFromLocalStorage === 'string' && userIpFromLocalStorage !== '') {
-
-        } else {
+        if (typeof userIpFromLocalStorage !== 'string' || userIpFromLocalStorage !== '') {
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange = function() { 
                 if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
