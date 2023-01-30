ac84595185446865e09f2d379861deed554d55df
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2023-01-30 17:40:24 +0100

    tmsConfig_alternateLanguageHierarchy
    added tmsConfig_alternateLanguageHierarchy to TMSConfig to ensure to always collect the de alternate tag

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 window.TMSConfig['tmsConfig_event_trackDefault'] = true;
 window.TMSConfig['tmsConfig_cookieDomain'] = 'projuventute.ch';
+window.TMSConfig['tmsConfig_alternateLanguageHierarchy'] = 'de'; // 2023-01-30 martin: added to ensure to always collect the de alternate tag
